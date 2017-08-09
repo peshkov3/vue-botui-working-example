e00f45d019d2caf3d98d8675a85be8c20cab0fcb
Max Peshkov <peshkov1.max@gmail.com>
2017-08-09 10:25:11 +0600

    Import botui file

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,22 +10,7 @@
     </div>
 </template>
 <script>
-  import Vue from 'vue'
-  import BotUI from '../../node_modules/botui/build/botui.js'
-  import '../../node_modules/botui/build/botui.min.css'
-  import '../../node_modules/botui/build/botui-theme-default.css'
+   let obj = require('../my-botui-file')
 
-  export default {
-    name: 'main',
-    mounted () {
-      let botui = BotUI('my-botui-app', {vue: Vue})
-      botui.message.add({content: 'Hello World from bot!'}).then(function () {
-        botui.message.add({
-          delay: 1000,
-          human: true,
-          content: 'Hello World from human!'
-        })
-      })
-    }
-  }
+   export default obj.default
 </script>
